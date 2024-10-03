be453e0882e95e91dfaeaac235735045c1c7548e
timer-master <lihengliu787@gmail.com>
2024-10-02 17:47:49 -0700

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,8 +1,3 @@
-// Dropdown Menu
-function reveal(id) {
-  const element = document.getElementById(id);
-  element.style.display = element.style.display == "none" ? "" : "none";
-}
 
 //COUNTDOWN TIMER
 // Timer Constants
