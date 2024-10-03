a13787e6d2e5d9759a25636ff29274aa80290867
kevinlu898 <kevinlu2882@gmail.com>
2024-10-02 18:02:59 -0700

    Update tips.js

--- tips.js
+++ tips.js
@@ -55,7 +55,7 @@ function setTip() {
   const ll = timeManagementTips.length;
   const a = getRandomInt(1, ll - 1);
   const b = timeManagementTips[a];
-  document.getElementById("tipid").innerHTML = b;
+  document.getElementById("tipid").innerHTML = "Daily Tip: " + b;
 }
 
 setTip();
