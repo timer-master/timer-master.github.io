0e8a79f2836f351d30e88dfe93ad8cda1d14f745
notAHuman123 <yl18130338@gmail.com>
2024-10-09 11:08:35 -0700

    h

--- timer.js
+++ timer.js
@@ -56,7 +56,7 @@ function startTimer() {
     timerInterval = setInterval(() => {
     timeLeft = TIME_LIMIT - timePassed;
     // Only increment timePassed if not paused
-    if (!isPaused && timeLeft != 0) {
+    if (!isPaused && timePassed == 0) {
       timePassed += 1;
     }
     document.getElementById("base-timer-label").innerHTML =
