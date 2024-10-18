170a4d14ce7304ea95c18544a6c6040f687a2efe
Bob Jagwires <lihengliu787@gmail.com>
2024-10-18 15:55:22 -0700

    time planner js without time setting

--- time-planner.js
+++ time-planner.js
@@ -5,64 +5,109 @@ function createCloseButton(listItem) {
     span.className = "close";
     span.appendChild(txt);
     listItem.appendChild(span);
-  
+
     // Click on a close button to hide the current list item
-    span.onclick = function() {
+    span.onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
     };
-  }
-  
-  // Add a "checked" symbol when clicking on a list item
-  var list = document.querySelector('ul');
-  list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-      ev.target.classList.toggle('checked');
-    }
-  }, false);
-  
-  // Create a new list item when clicking on the "Add" button
-  function newElement() {
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector("ul");
+list.addEventListener(
+    "click",
+    function (ev) {
+      if (ev.target.tagName === "LI") {
+        ev.target.classList.toggle("checked");
+      }
+    },
+    false
+);
+
+// Convert time string (24-hour) to 12-hour format with AM/PM
+function convertTo12HourFormat(timeString) {
+    var [hour, minute] = timeString.split(":");
+    hour = parseInt(hour);
+
+    var period = hour >= 12 ? "PM" : "AM";
+    hour = hour % 12 || 12;
+
+    return hour + ":" + minute + " " + period;
+}
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
     var li = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
-    var taskStart = document.getElementById("taskStart").value;
-    var taskEnd = document.getElementById("taskEnd").value;
+    var taskStart = document.getElementById("taskStart").value; // Date and Time
+    var taskEnd = document.getElementById("taskEnd").value;     // Date and Time
 
-  
-    // Display task text and time
-    if (inputValue === '') {
+    // Validate input fields
+    if (inputValue === "") {
       alert("You must describe your task.");
-      return; 
-    } 
-    if (taskStart === '') {
-      alert("You must set a start time.");
       return;
     }
-    if (taskEnd === '') {
-        alert("You must set an end time.");
-        return; 
-      }
-  
-    var t = document.createTextNode(inputValue + "  Start: " + taskStart + "  End: " + taskEnd);
+    if (taskStart === "") {
+      alert("You must set a start date and time.");
+      return;
+    }
+    if (taskEnd === "") {
+      alert("You must set an end date and time.");
+      return;
+    }
+
+    // Convert the date and time strings into Date objects
+    var startDateTime = new Date(taskStart);
+    var endDateTime = new Date(taskEnd);
+
+    // Check if the end time is after the start time
+    if (endDateTime <= startDateTime) {
+      alert("The end date and time must be after the start date and time.");
+      return;
+    }
+
+    // Calculate the duration in hours
+    var durationInMilliseconds = endDateTime - startDateTime;
+    var durationInHours = durationInMilliseconds / (1000 * 60 * 60);
+
+    // Check if the duration is less than 24 hours
+    if (durationInHours > 24) {
+      alert("Tasks should generally be completed in less than 24 hours. To set long-term tasks, use our To Do feature.");
+      return;
+    }
+
+    // Convert the times to 12-hour format
+    var taskStart12Hour = convertTo12HourFormat(startDateTime.toTimeString().slice(0, 5));
+    var taskEnd12Hour = convertTo12HourFormat(endDateTime.toTimeString().slice(0, 5));
+
+    // Display task with start and end times (without date)
+    var t = document.createElement("span");
+    t.innerHTML =
+      "<b>" +
+      inputValue +
+      "</b>" +
+      '<span style="margin-left: 30px;"><b>Start: </b>' +
+      taskStart12Hour +
+      "</span>" +
+      '<span style="margin-left: 30px;"><b>End: </b>' +
+      taskEnd12Hour +
+      "</span>";
     li.appendChild(t);
-    
+
+    // Timer icon
+    var timerIcon = document.createElement("span");
+    timerIcon.className = "material-symbols-outlined timer-icon";
+    timerIcon.textContent = "timer";
+    li.appendChild(timerIcon);
+
     document.getElementById("myUL").appendChild(li);
-    
+
     // Create close button for the new list item
     createCloseButton(li);
-    
-    // Timer icon
-    var alarmIcon = document.createElement("span");
-    alarmIcon.className = "material-symbols-outlined alarm-icon";
-    alarmIcon.textContent = "alarm";
-    li.appendChild(alarmIcon);
-    
-    // Start Timer
-  
+
     // Clear input fields
     document.getElementById("myInput").value = "";
     document.getElementById("taskStart").value = "";
     document.getElementById("taskEnd").value = "";
-  }
-  
-  
+}
