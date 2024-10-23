56e5fe1ea8057676ffa3ffd0552ab85b24cfcd06
Bob Jagwires <lihengliu787@gmail.com>
2024-10-22 21:10:48 -0700

    index

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ function submit() {
   alert("Submit reflection?");
 }
 
+// Index typing
+
+var i = 0;
+var txt = 'Time management is a challenge for students and adults alike in today’s fast-paced world, often leading to stress and decreased productivity. But not to worry, Timer Master is here to help. With various proven time management, we can help you increase your efficiency and maximize your time. Experience the benefits of effective time management with Timer Master and unlock a more productive, fulfilling life.';
+var speed = 40;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("text").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter();
